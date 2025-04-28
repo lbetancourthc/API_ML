6d55af5d0d6e2439aa427db2a998d792fccf550f
Landneyker <82390922+lbetancourthc@users.noreply.github.com>
2025-04-28 16:41:39 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,18 @@
 # api.py
 from fastapi import FastAPI
 from pydantic import BaseModel
-import joblib
 import numpy as np
+import joblib
+import gdown
+import os
 
 # Cargar el modelo entrenado
-best_rf = joblib.load('best_rf_model.pkl')
+model_url = "https://drive.google.com/uc?id=1wQ9k4wTuGr0r5HNyucNvRSgvkyCtosxn"
+
+if not os.path.exists("best_rf_model.pkl"):
+    gdown.download(model_url, "best_rf_model.pkl", quiet=False, fuzzy=True)
+
+best_rf = joblib.load("best_rf_model.pkl")
 
 # Crear app
 app = FastAPI()
